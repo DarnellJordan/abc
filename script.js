666bed579d1d73405a46e444841508aa1468330a
psyGamer <56753271+psyGamer@users.noreply.github.com>
2021-07-10 14:22:46 +0200

    Changed Stuff

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-const pageElements = [...document.querySelector(".markdown-body").children].slice(2)
+const pageElements = [...document.querySelector(".markdown-body").children].slice(2);
 
-document.body.innerHTML = ""
+document.body.innerHTML = "";
 pageElements.forEach(pageElement => {
 	document.body.appendChild(pageElement)
-})
+});
+
+[...document.head.children].pop().href = "https://psygamer.github.io/abc/style.css";
